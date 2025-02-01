ba82e918b1c28778455eb3049bd28113d36b1524
matib <matibmateusz@gmail.com>
2025-02-01 14:24:15 +0100

    Komenda loss i kolejne poprawki

--- main.py
+++ main.py
@@ -18,43 +18,53 @@ async def on_ready():
     except Exception as e:
         print("An error with syncing application commands has occurred: ",e)
 
+@bot.tree.command(name="loss", description="Is this Loss?")
+async def loss(interaction: discord.Interaction):
+    embed = discord.Embed(
+        title="Loss przepowiedział",
+        description="~~:.|:;~~",
+        color=discord.Color.blue()
+    )
+    await interaction.response.send_message(embed=embed)
+
+
 @bot.tree.command(name="los", description="Tutaj zadecyduje się twoje przeznaczenie!")
 async def los(interaction: discord.Interaction):
     liczba = random.randint(1, 100)
     
     match liczba:
         case 1:
-            kolor = discord.color.gold()
-            opis = "**Krytyczny Sukces!**"
+            kolor = discord.Color.gold()
+            opis = "**1 - Krytyczny Sukces!**"
         case 4:
-            kolor = discord.color.blue()
-            opis = "**Błogosławieństwo!**"
+            kolor = discord.Color.blue()
+            opis = "**4 - Błogosławieństwo!**"
         case 7:
-            kolor = discord.color.red()
+            kolor = discord.Color.red()
             opis = "**7 - Pech!**"
         case 8:
-            kolor = discord.color.green()
+            kolor = discord.Color.green()
             opis = "**8 - Szczęście**"
         case 12:
-            kolor = discord.color.dark_gold()
+            kolor = discord.Color.dark_gold()
             opis = "**12 - Siła!**"
         case 44:
-            kolor = discord.color.dark_blue()
+            kolor = discord.Color.dark_blue()
             opis = "**44 - Dar od Bogów**"
         case 69:
-            kolor = discord.color.pink()
+            kolor = discord.Color.pink()
             opis = ":heart_eyes: **69 - Miłość!** :heart_eyes:"
         case 77:
-            kolor = discord.color.dark_red()
+            kolor = discord.Color.dark_red()
             opis = "**77 - Klątwa Losa!**"
         case 88:
-            kolor = discord.color.dark_green()
+            kolor = discord.Color.dark_green()
             opis = "**88 - Dar od Losa!**"
         case 100:
-            kolor = discord.color.default()
+            kolor = discord.Color.default()
             opis = "**100 - Krytyczna Porażka**"
         case default:
-            kolor = discord.color.dark_gray()
+            kolor = discord.Color.dark_gray()
             opis = f"**{liczba} - Nic się nie wydarzy**"
 
     embed = discord.Embed(
